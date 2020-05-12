ecd1792982884e567798906caa3490f7e0931f77
Joel Santana <p3t3ca5206@gmail.com>
2020-05-12 04:56:13 -0300

    created header

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400&display=swap" rel="stylesheet"><link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400&display=swap" rel="stylesheet">        
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+        <title>Awesome</title>
+    </head>
+
+    <body>
+        <header>
+
+            <div class="header-top">
+                <div class="aligment">
+                    <div class="header-top-content">
+
+                        <div class="contacts">
+                            <img src="assets/img/ligar.png" />
+                            <h3> Phone <span><a href="#"> 010-020-0340 </a></span> </h3>
+                        </div>
+
+                        <div class="contacts">
+                            <img src="assets/img/multimidia.png" width="19px" height="18px"/>
+                            <h3> Email <h4><a href="#">awesome@company.com </a></h4> </h3>
+                        </div>
+                        
+                        <div class="social-networks">
+                            <h3>Meet us on</h3>
+                            <div class="icon-face"></div>
+                            <div class="icon-tw"></div>
+                            <div class="icon-insta"></div>
+                            <div class="icon-apple"></div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="header-bottom">
+                <div class="aligment">
+                    <div class="header-bottom-content">
+
+                        <div class="logo">
+                            <h1>Awesome</h1>
+                        </div>
+                    <div class="menu">   
+                        <nav>
+                            <ul>
+                                <li><a href="">home</a></li>
+                                <li><a href="">about</a></li>
+                                <li><a href="">team</a></li>
+                                <li><a href="">service</a></li>
+                                <li><a href="">portfolio</a></li>
+                                <li><a href="">contact</a></li>
+                            </ul>
+                        </nav>
+                    </div>  
+                    </div>
+                </div>
+            </div>
+
+        </header>
+    </body>
+
+</html>

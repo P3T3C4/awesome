643baa76282539b715758ac3f4be4ef8e06ffbc2
Joel Santana <p3t3ca5206@gmail.com>
2020-05-22 12:56:26 -0300

    finished front project

--- index.php
+++ index.php
@@ -44,23 +44,391 @@
                         <div class="logo">
                             <h1>Awesome</h1>
                         </div>
-                    <div class="menu">   
-                        <nav>
-                            <ul>
-                                <li><a href="">home</a></li>
-                                <li><a href="">about</a></li>
-                                <li><a href="">team</a></li>
-                                <li><a href="">service</a></li>
-                                <li><a href="">portfolio</a></li>
-                                <li><a href="">contact</a></li>
-                            </ul>
-                        </nav>
-                    </div>  
+
+                        <div class="menu">   
+                            <nav>
+                                <ul>
+                                    <li><a href="" class="active">home</a></li>
+                                    <li><a href="">about</a></li>
+                                    <li><a href="">team</a></li>
+                                    <li><a href="">service</a></li>
+                                    <li><a href="">portfolio</a></li>
+                                    <li><a href="">contact</a></li>
+                                </ul>
+                            </nav>
+                        </div>  
+                        
                     </div>
                 </div>
             </div>
 
         </header>
+
+        <main>
+
+            <section class="banner">
+                <div class="shadow">
+                    <div class="slides">
+
+                        <div class="slide">
+                            <div class="slide-content">
+                                <h1>We make templates</br>that are <span>awesome</span></h1>
+                                <h4>Awesome Template Is provided by templatemo.com website for everyone</h4>
+
+                                <button> Get Started </button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+           <section class="contents">
+                <div class="aligment">
+                    <div class="general-content">
+                        <div class="general-title">
+                            <h1>we are <span>awesome</span></h1>
+                        </div>
+                        <div class="general-content-int">
+                            <div class="flex-boxes">
+                                <div class="box-skill">
+
+                                    <div class="box-skill-icons">
+                                        <img src="assets/img/smartphone.png" />
+                                    </div>
+
+                                    <div class="box-skill-content">
+                                        <h1>fully responsive</h1>
+
+                                        <h4>
+                                            Awesome responsive template is provided 
+                                            </br>by <span>templatemo</span> website. This is Bootstrap 
+                                            </br>v3.3.2 layout built on HTML5 CSS3. You can 
+                                            </br> use this for any purpose. Plese tell your 
+                                            </br> friends about it. 
+                                        </h4>
+                                    </div>
+
+                                </div>
+                                <div class="box-skill">
+                                    
+                                    <div class="box-skill-icons">
+                                        <img src="assets/img/bate-papo.png" />
+                                    </div>
+
+                                    <div class="box-skill-content">
+                                        <h1>free support</h1>
+
+                                        <h4>
+                                            Awesome responsive template is provided 
+                                            </br>by <span>templatemo</span> website. This is Bootstrap 
+                                            </br>v3.3.2 layout built on HTML5 CSS3. You can 
+                                            </br> use this for any purpose. Plese tell your 
+                                            </br> friends about it. 
+                                        </h4>
+                                    </div>
+
+                                </div>
+                                <div class="box-skill">
+
+                                    <div class="box-skill-icons">
+                                        <img src="assets/img/cracha.png" />
+                                    </div>
+
+                                    <div class="box-skill-content">
+                                        <h1>HTML5  &  CSS3</h1>
+
+                                        <h4>
+                                            Awesome responsive template is provided 
+                                            </br>by <span>templatemo</span> website. This is Bootstrap 
+                                            </br>v3.3.2 layout built on HTML5 CSS3. You can 
+                                            </br> use this for any purpose. Plese tell your 
+                                            </br> friends about it. 
+                                        </h4>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+           </section> 
+           
+           <section class="contents">
+                <div class="aligment">
+                    <div class="general-content">
+                        <div class="general-title">
+                            <h1><span>awesome</span> team</h1>
+                        </div>
+                        <div class="general-content-int">
+                            <div class="flex-boxes">
+
+                                <div class="team">
+                                    <div class="person-photo">
+                                        <img src="assets/img/dev1.jpg" />
+                                    </div>
+                                    <div class="team-description">
+
+                                        <h1>tracy</h1>
+
+                                        <span>Designer</span>
+
+                                        <h4>
+                                            Lorem Ipsum dolor sit amet,</br>
+                                            consectetur adipscing elitquisque </br>
+                                            tempus ac eget diam et laoreet </br> 
+                                            phasellus ut nisi id leo molest.
+                                        </h4>
+
+                                    </div>
+                                </div>
+
+                                <div class="team">
+                                    <div class="person-photo">
+                                        <img src="assets/img/dev2.jpg" />
+                                    </div>
+                                    <div class="team-description">
+
+                                        <h1>tracy</h1>
+
+                                        <span>Developer</span>
+
+                                        <h4>
+                                            Lorem Ipsum dolor sit amet,</br>
+                                            consectetur adipscing elitquisque </br>
+                                            tempus ac eget diam et laoreet </br> 
+                                            phasellus ut nisi id leo molest.
+                                        </h4>
+
+                                    </div>
+                                </div>
+
+                                <div class="team">
+                                    <div class="person-photo">
+                                        <img src="assets/img/dev3.jpg" />
+                                    </div>
+                                    <div class="team-description">
+
+                                        <h1>tracy</h1>
+
+                                        <span>Director</span>
+
+                                        <h4>
+                                            Lorem Ipsum dolor sit amet,</br>
+                                            consectetur adipscing elitquisque </br>
+                                            tempus ac eget diam et laoreet </br> 
+                                            phasellus ut nisi id leo molest.
+                                        </h4>
+
+                                    </div>
+                                </div>
+
+                                <div class="team">
+                                    <div class="person-photo">
+                                        <img src="assets/img/dev4.jpg" />
+                                    </div>
+                                    <div class="team-description">
+
+                                        <h1>tracy</h1>
+
+                                        <span>Manager</span>
+
+                                        <h4>
+                                            Lorem Ipsum dolor sit amet,</br>
+                                            consectetur adipscing elitquisque </br>
+                                            tempus ac eget diam et laoreet </br> 
+                                            phasellus ut nisi id leo molest.
+                                        </h4>
+
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+           </section> 
+
+           <section class="contents">
+               <div class="aligment">
+                   <div class="general-content">
+                       <div class="general-title">
+                            <h1>our <span>awesome</span> things</h1>
+                       </div>
+                       <div class="general-content-int">
+                           <div class="flex-boxes">
+                                <div class="our-things">
+
+                                    <div class="our-things-icon">
+                                        <img src="assets/img/ferramentas.png" />
+                                    </div>
+
+                                    <h1>Web Design<h1>
+
+                                    <h4>
+                                        Lorem ipsum dolor sit amet, consectetur </br>
+                                        adipiscing elitquisque tempus ac eget diam et </br>
+                                        laoreet phasellus ut nisi id leo molestie. </br>
+                                        Adipiscing vitae vel quam proin eget mauris eget. </br>
+                                        Lorem ipsum dolor sit amet, consectetur </br>
+                                        adipiscing elitquisque tempus ac eget diam et </br>
+                                        laoreet phasellus ut nisi is leo molestie.
+                                    </h4>
+
+                                </div>
+                                <div class="our-things">
+                                    <div class="our-things-icon">
+                                        <img src="assets/img/nublado.png" />
+                                    </div>
+
+                                    <h1>Cloud Computing<h1>
+
+                                    <h4>
+                                        Lorem ipsum dolor sit amet, consectetur </br>
+                                        adipiscing elitquisque tempus ac eget diam et </br>
+                                        laoreet phasellus ut nisi id leo molestie. </br>
+                                        Adipiscing vitae vel quam proin eget mauris eget. </br>
+                                        Lorem ipsum dolor sit amet, consectetur </br>
+                                        adipiscing elitquisque tempus ac eget diam et </br>
+                                        laoreet phasellus ut nisi is leo molestie.
+                                    </h4>
+
+                                </div>
+                                <div class="our-things">
+                                    <div class="our-things-icon">
+                                        <img src="assets/img/simbolo.png" />
+                                    </div>
+
+                                    <h1>UX Design<h1>
+
+                                    <h4>
+                                        Lorem ipsum dolor sit amet, consectetur </br>
+                                        adipiscing elitquisque tempus ac eget diam et </br>
+                                        laoreet phasellus ut nisi id leo molestie. </br>
+                                        Adipiscing vitae vel quam proin eget mauris eget. </br>
+                                        Lorem ipsum dolor sit amet, consectetur </br>
+                                        adipiscing elitquisque tempus ac eget diam et </br>
+                                        laoreet phasellus ut nisi is leo molestie.
+                                    </h4>
+
+                                </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </section>
+           
+           <section class="contents">
+                <div class="aligment">
+                    <div class="general-content">
+                        <div class="general-title">
+                            <h1><span>awesome</span> portfolio</h1>
+                        </div>
+                        <div class="general-content-int">
+                            <div class="portfolio">
+                                <div class="portfolio-top">
+                                    <div class="portfolio-photos">
+                                        <img src="assets/img/photo1.jpeg" />
+                                    </div>
+                                    <div class="portfolio-photos">
+                                        <img src="assets/img/photo2.jpeg" />
+                                    </div>
+                                    <div class="portfolio-photos">
+                                        <img src="assets/img/photo3.jpeg" />
+                                    </div>
+                                    <div class="portfolio-photos">
+                                        <img src="assets/img/photo4.jpeg" />
+                                    </div>
+                                </div>
+                                <div class="portfolio-bottom">
+                                    <div class="portfolio-photos">
+                                        <img src="assets/img/photo5.jpeg" />
+                                    </div>
+                                    <div class="portfolio-photos">
+                                        <img src="assets/img/photo6.jpeg" />
+                                    </div>
+                                    <div class="portfolio-photos">
+                                        <img src="assets/img/photo7.jpeg" />
+                                    </div>
+                                    <div class="portfolio-photos">
+                                        <img src="assets/img/photo8.jpeg" />
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+           </section>
+           
+           <section class="contents">
+                <div class="aligment">
+                    <div class="general-content">
+                        <div class="general-title">
+                            <h1>contact <span>awesome</span></h1>
+                        </div>
+                    </div>
+                    <div class="general-content-int">
+                        <div class="contact">
+                            <div class="contact-left">
+                                name
+                                <input type="text" name="name"/>
+                                email
+                                <input type="email" name="email" />
+                                message
+                                <input type="text" name="message" />
+
+                                <button>Submit Query</button>
+                            </div>
+                            <div class="contact-right">
+                                <div class="our-address">
+                                    <h1>Our address</h1>
+                                    <h4> 
+                                        Lorem ipsum dolor sit amet, consectetur 
+                                        adipiscing elitquisque tempus ac eget diam et </br> 
+                                        laoreet phasellus ut nisi id leo molestie.
+                                    </h4>
+
+                                    <div class="our-address-contact">
+                                        <div class="our-address-contact-icon">
+                                            <img src="assets/img/telefone-bottom.png" />
+                                        </div>
+                                        010-020-0340
+                                    </div>
+                                    <div class="our-address-contact">
+                                        <div class="our-address-contact-icon">
+                                            <img src="assets/img/enviar (1).png" />
+                                        </div>
+                                        awesome@company.com
+                                    </div>
+                                    <div class="our-address-contact">
+                                        <div class="our-address-contact-icon">
+                                            <img src="assets/img/pin.png" />
+                                        </div>
+                                        663 New Walk Roadside, Birdeye View,GO 11020
+                                    </div>
+                                </div>
+                                <div class="social-bottom">
+                                    <h1>we are social</h1>
+                                    <div class="social-bottom-icons">
+                                        <img src="assets/img/facebook (1).png" />
+                                    </div>
+                                    <div class="social-bottom-icons">
+                                        <div class="social-bottom-icons-tweet"></div>
+                                    </div>
+                                    <div class="social-bottom-icons">
+                                        <img src="assets/img/instagram (1).png" />
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+           </section>
+            
+        </main>
+
     </body>
 
 </html>
